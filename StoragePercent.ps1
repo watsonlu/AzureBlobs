29a5ec6ad9af2ec6749386f8978c7fed375b8c68
Luke Watson <luwatson90@gmail.com>
2017-01-19 01:36:53 -0500

    Working Demo

--- StoragePercent.ps1
+++ StoragePercent.ps1
@@ -9,21 +9,44 @@ $ResourceGroup = "myresourcegrou"
 $StorageKey = 0
 $DirectoryToUpload = "C:\Users\luwat\Documents\Projects\Powershell\AzureBlobs\images"
 
-#TODO: Do this programatically
-#$account = Add-AzureAccount
-#Set-AzureSubscription -SubscriptionName "Visual Studio Enterprise" -CurrentStorageAccountName $StorageAccountName
-Login-AzureRmAccount
-$account = Get-AzureRMStorageAccount -StorageAccountName $StorageAccountName -ResourceGroupName $ResourceGroup
+
+
+Get-AuthenticatedWithAzure -SubscriptionName $SubscriptionName
 $key = (Get-AzureRMStorageAccountKey -ResourceGroup $ResourceGroup -StorageAccountName $StorageAccountName)[$StorageKey].value
 $context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $key
 
 
 #Upload some garbage
 
-Upload-TestBlobs -ContainerName $ContainerName -DirectoryToUpload $DirectoryToUpload
+Upload-TestBlobs -ContainerName $ContainerName -DirectoryToUpload $DirectoryToUpload -Context $context
+$blobs = Get-AzureStorageBlob -Container $ContainerName -Context $context | sort Length -Descending
+$TopIndexes = Get-TopPercentageOfIndexes -array $blobs -PercentageOf 5
+$blobs | Select-Object -First $TopIndexes
 
-#Get-AzureStorageContainer -Name $ContainerName -Context ()
-Get-AzureStorageBlob -Container $ContainerName -Context $context
+function Get-AuthenticatedWithAzure
+{
+    Param(
+        [string]$SubscriptionName
+    )  
+    Try
+    {
+        Set-AzureSubscription -SubscriptionName $SubscriptionName
+    }
+    Catch{
+        Add-AzureRmAccount
+        Set-AzureSubscription -SubscriptionName $SubscriptionName
+    }
+}
+
+
+function Get-TopPercentageOfIndexes
+{
+    Param(
+        [array]$array,
+        [int]$PercentageOf
+    )   
+    return [math]::Ceiling(($PercentageOf * $array.Count) / 100)
+}
 
 
 function Upload-TestBlobs
@@ -31,13 +54,10 @@ function Upload-TestBlobs
     Param(
         [string]$ContainerName,
         [string]$DirectoryToUpload,
-        [string]$Context
+        [Microsoft.WindowsAzure.Commands.Common.Storage.AzureStorageContext]$Context
     )
-    $images = Get-ChildItem $DirectoryToUpload
-    foreach ($image in $images) {
-        $BlobName = $image 
-        $localFile = $image.FullName
-        Set-AzureStorageBlobContent -File $localFile -Container $ContainerName -Blob $BlobName -Context $Context
+    foreach ($image in Get-ChildItem $DirectoryToUpload) {
+        #Set-AzureStorageBlobContent -File $image.FullName -Container $ContainerName -Blob $image.Name -Context $Context -Force
     }
 
 }
